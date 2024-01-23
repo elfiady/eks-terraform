4efcb79066716f1fcaf120bf649b1eacc5081be3
elfiady <133861131+elfiady@users.noreply.github.com>
2024-01-23 14:30:47 +0100

    Update vpc.tf
    test

--- vpc.tf
+++ vpc.tf
@@ -17,7 +17,7 @@ resource "random_string" "suffix" {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "2.66.0"
+  version = "3.19.0"
 
   name                 = "devops-ninja-vpc"
   cidr                 = "10.0.0.0/16"
