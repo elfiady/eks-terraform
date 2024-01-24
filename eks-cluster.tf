c44b4354f15bde44bbca428db6585cf98233fd72
elfiady <133861131+elfiady@users.noreply.github.com>
2024-01-24 14:47:03 +0100

    Update eks-cluster.tf
    test

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -1,36 +1,44 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version = "~> 19.16"
+  version         = "17.24.0"
   cluster_name    = local.cluster_name
   cluster_version = "1.28"
-  
+  subnets         = module.vpc.public_subnets
   tags = {
     Environment = "training"
     GithubRepo  = "terraform-aws-eks"
     GithubOrg   = "terraform-aws-modules"
   }
-
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-  create_node_security_group    = false
-
-  eks_managed_node_groups = {
-    default = {
-      instance_types       = ["m5.large"]
-      force_update_version = true
-      release_version      = var.ami_release_version
-
-      min_size     = 3
-      max_size     = 6
-      desired_size = 3
-      
-      update_config = {
-        max_unavailable_percentage = 50
-      }
-
-      labels = {
-        workshop-default = "yes"
-      }
-    }
+  vpc_id = module.vpc.vpc_id
+  workers_group_defaults = {
+    root_volume_type = "gp2"
   }
+  worker_groups = [
+    {
+      name                          = "worker-group-1"
+      instance_type                 = "t2.small"
+      additional_userdata           = "echo foo bar"
+      asg_desired_capacity          = 1
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+    },
+    {
+      name                          = "worker-group-2"
+      instance_type                 = "t2.medium"
+      additional_userdata           = "echo foo bar"
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+      asg_desired_capacity          = 1
+    },
+  ]
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_id
+}
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
+}
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
