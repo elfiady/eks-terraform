0c4781bd255f78bf7331f3d14394d8f61876d452
elfiady <133861131+elfiady@users.noreply.github.com>
2024-01-24 11:23:56 +0100

    Update eks-cluster.tf
    test

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -13,28 +13,25 @@ module "eks" {
   vpc_id = module.vpc.vpc_id
 
  workers_group_defaults = {
-    root_volume_type = "gp2"
-  }
+    root_volume_type = "gp2"
+  }
 
-  worker_groups = [
-    {
-      name                          = "worker-group-1"
-      instance_type                 = "t2.small"
-      additional_userdata           = "echo foo bar"
-      asg_desired_capacity          = 1
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
-    },
-    {
-      name                          = "worker-group-2"
-      instance_type                 = "t2.medium"
-      additional_userdata           = "echo foo bar"
-      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
-      asg_desired_capacity          = 1
-    },
-  ]
-}
-data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+  worker_groups = [
+    {
+      name                          = "worker-group-1"
+      instance_type                 = "t2.small"
+      additional_userdata           = "echo foo bar"
+      asg_desired_capacity          = 1
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_one.id]
+    },
+    {
+      name                          = "worker-group-2"
+      instance_type                 = "t2.medium"
+      additional_userdata           = "echo foo bar"
+      additional_security_group_ids = [aws_security_group.worker_group_mgmt_two.id]
+      asg_desired_capacity          = 1
+    },
+  ]
 }
 
 data "aws_eks_cluster_auth" "cluster" {
